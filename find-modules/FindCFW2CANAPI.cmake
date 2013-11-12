ff5fccb6af18fec789fd649cc82211ea6bd870e2
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-11-12 16:09:45 +0100

    Cleanup

--- find-modules/FindCFW2CANAPI.cmake
+++ find-modules/FindCFW2CANAPI.cmake
@@ -1,32 +1,28 @@
-# Copyright: (C) 2010 RobotCub Consortium
-# Authors: Lorenzo Natale
-# CopyPolicy: Released under the terms of the GNU GPL v2.0.
-
 # Created:
 # CFW2CANAPI_INC_DIRS   - Directories to include to use esdcan api
 # CFW2CANAPI_LIB        - Default library to link against to use the esdcan API
 # CFF2CANAPI_FOUND      - If false, don't try to use esdcan API
 
-IF(NOT CFW2CANAPI_FOUND)
-    SET(CFW2CANAPI_DIR $ENV{CFW2CANAPI_DIR} CACHE PATH "Path to CFW2CANAPI")
-    SET(CFW2CANAPI_INC_DIRS ${CFW2CANAPI_DIR}/LinuxDriver/API)
-    
-    IF(WIN32)
+# Copyright: (C) 2010 RobotCub Consortium
+# Authors: Lorenzo Natale
+# CopyPolicy: Released under the terms of the GNU GPL v2.0.
+
+if(NOT CFW2CANAPI_FOUND)
+    set(CFW2CANAPI_DIR $ENV{CFW2CANAPI_DIR} CACHE PATH "Path to CFW2CANAPI")
+    set(CFW2CANAPI_INC_DIRS ${CFW2CANAPI_DIR}/LinuxDriver/API)
+
+    if(WIN32)
        #sorry not available in windows
-    ELSE(WIN32)  
-       FIND_LIBRARY(CFW2CANAPI_LIB cfw002 ${CFW2CANAPI_DIR}/LinuxDriver/API)
-    ENDIF(WIN32)
-    
-    IF(CFW2CANAPI_LIB)
-       SET(CFW2CANAPI_FOUND TRUE)
-    ELSE(CFW2CANAPI_LIB)
-       SET(CFW2CANAPI_FOUND FALSE)
-       SET(CFW2CANAPI_INC_DIRS)
-       SET(CFW2CANAPI_LIB )
-    ENDIF(CFW2CANAPI_LIB)
+    else()
+        find_library(CFW2CANAPI_LIB cfw002 ${CFW2CANAPI_DIR}/LinuxDriver/API)
+    endif()
 
-ENDIF(NOT CFW2CANAPI_FOUND)
+    if(CFW2CANAPI_LIB)
+       set(CFW2CANAPI_FOUND TRUE)
+    else()
+       set(CFW2CANAPI_FOUND FALSE)
+       set(CFW2CANAPI_INC_DIRS)
+       set(CFW2CANAPI_LIB )
+    endif(CFW2CANAPI_LIB)
 
-
-
-
+endif(NOT CFW2CANAPI_FOUND)
