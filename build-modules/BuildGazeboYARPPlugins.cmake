1a7903c1a2961156450ff16170b44f64860cda1f
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-11-19 13:32:10 +0100

    BuildGazeboYARPPlugins: Find gazebo quietly

--- build-modules/BuildGazeboYARPPlugins.cmake
+++ build-modules/BuildGazeboYARPPlugins.cmake
@@ -5,7 +5,7 @@ include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
 find_or_build_package(ICUB QUIET)
-find_package(gazebo)
+find_package(gazebo QUIET)
 
 ycm_ep_helper(GazeboYARPPlugins TYPE GIT
                                 STYLE GITHUB
