4651260fbed71536f1ceacb658fb5f21b788339f
Lorenzo Natale <lorenzo.natale@iit.it>
2013-11-29 18:47:57 +0100

    added build rule

---
+++ build-modules/Buildfakebot.cmake
@@ -0,0 +1,13 @@
+# TemplateExe
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP REQUIRED)
+
+ycm_ep_helper(fakebot TYPE GIT
+                          STYLE GITHUB
+                          REPOSITORY lornat75/fakebot.git
+                          TAG master
+                          DEPENDS YARP)
+
+
