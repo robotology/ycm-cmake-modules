4b793e2986b4e9778ae5a7c13974ccc8ac41c4e4
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-12-02 12:01:30 +0100

    Cleanup

--- build-modules/Buildfakebot.cmake
+++ build-modules/Buildfakebot.cmake
@@ -5,9 +5,7 @@ include(FindOrBuildPackage)
 find_or_build_package(YARP REQUIRED)
 
 ycm_ep_helper(fakebot TYPE GIT
-                          STYLE GITHUB
-                          REPOSITORY lornat75/fakebot.git
-                          TAG master
-                          DEPENDS YARP)
-
-
+                           STYLE GITHUB
+                           REPOSITORY lornat75/fakebot.git
+                           TAG master
+                           DEPENDS YARP)
