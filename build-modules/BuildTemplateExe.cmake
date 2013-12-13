a620e10f22b87d2e572ee3d3f667cf3022096236
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-12-13 15:58:33 +0100

    BuildTemplateExe: Use TemplatePkg

--- build-modules/BuildTemplateExe.cmake
+++ build-modules/BuildTemplateExe.cmake
@@ -2,11 +2,11 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-find_or_build_package(TemplateLib QUIET)
+find_or_build_package(TemplatePkg QUIET)
 
 ycm_ep_helper(TemplateExe TYPE GIT
                           STYLE GITLAB_ICUB_ORG
                           COMPONENT templates
                           REPOSITORY walkman/template-exe.git
                           TAG master
-                          DEPENDS TemplateLib)
+                          DEPENDS TemplatePkg)
