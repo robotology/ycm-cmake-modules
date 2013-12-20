22d96d83b5e3813cc5cc99986b6af6ff5ddf6ac6
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-12-20 12:36:04 +0100

    Add BuildComanSimulator.cmake

---
+++ build-modules/BuildComanSimulator.cmake
@@ -0,0 +1,9 @@
+# ComanSimulator
+
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+ycm_ep_helper(ComanSimulator TYPE GIT
+                             STYLE GITHUB
+                             REPOSITORY HDallali/ComanSimulator.git
+                             TAG master)
