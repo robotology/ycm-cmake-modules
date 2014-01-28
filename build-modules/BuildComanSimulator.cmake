2073f9dba1ac024b7070cc260313f1f948c5e781
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-01-28 15:36:01 +0100

    BuildComanSimulator: require LibXml2

--- build-modules/BuildComanSimulator.cmake
+++ build-modules/BuildComanSimulator.cmake
@@ -3,6 +3,8 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
+find_package(LibXml2 REQUIRED)
+
 ycm_ep_helper(ComanSimulator TYPE GIT
                              STYLE GITHUB
                              REPOSITORY HDallali/ComanSimulator.git
