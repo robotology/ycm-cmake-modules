c9de0a39a0d52721ad04a6ed3ccec8912c83bae1
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-02-03 15:57:00 +0100

    Remove build files for templates and fakebot

--- build-modules/BuildTemplatePkg.cmake
+++
@@ -1,8 +0,0 @@
-# TemplatePkg
-include(YCMEPHelper)
-
-ycm_ep_helper(TemplatePkg TYPE GIT
-                          STYLE GITLAB_ICUB_ORG
-                          REPOSITORY walkman/template-pkg.git
-                          TAG master
-                          COMPONENT templates)
