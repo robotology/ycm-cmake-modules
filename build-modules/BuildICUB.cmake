39b042cd3c98a36d93e3bc4487ef91a7b55f454f
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-02-03 16:02:12 +0100

    iCub main repository was moved to git

--- build-modules/BuildICUB.cmake
+++ build-modules/BuildICUB.cmake
@@ -4,9 +4,9 @@ include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
 
-ycm_ep_helper(ICUB TYPE SVN
-                   STYLE SOURCEFORGE
-                   REPOSITORY robotcub/code/trunk/iCub/main
+ycm_ep_helper(ICUB TYPE GIT
+                   STYLE GITHUB
+                   REPOSITORY robotology/icub-main.git
                    DEPENDS YARP
                    CMAKE_CACHE_ARGS -DYARP_USE_GTK2:BOOL=ON
                                     -DICUB_SHARED_LIBRARY:BOOL=ON
