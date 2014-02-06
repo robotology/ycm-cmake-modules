4822d5a8a118e1eef452c8e9e159f89febe160eb
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-02-06 17:21:16 +0100

    YCMEPHelper: Pass the CMAKE_PREFIX_PATH to the subprojects

--- modules/YCMEPHelper.cmake
+++ modules/YCMEPHelper.cmake
@@ -368,7 +368,7 @@ function(YCM_EP_HELPER _name)
                           STAMP_DIR ${${_name}_STAMP_DIR})
 
     set(${_name}_CMAKE_ARGS "--no-warn-unused-cli"
-                            "-DCMAKE_PREFIX_PATH:PATH=${${_name}_INSTALL_DIR}"    # Path used by cmake for finding stuff
+                            "-DCMAKE_PREFIX_PATH:PATH=$ENV{CMAKE_PREFIX_PATH};${${_name}_INSTALL_DIR}"    # Path used by cmake for finding stuff
                             "-DCMAKE_INSTALL_PREFIX:PATH=${${_name}_INSTALL_DIR}" # Where to do the installation
                             "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}"       # If there is a CMAKE_BUILD_TYPE it is important to ensure it is passed down.
                             "-DCMAKE_SKIP_RPATH:PATH=\"${CMAKE_SKIP_RPATH}\""
