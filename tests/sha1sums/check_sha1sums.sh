46d6544b892fce64bc196225a2cacc28506983ba
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-03-21 03:07:58 +0100

    Add unit test to check that sha1sums are correct

---
+++ tests/sha1sums/check_sha1sums.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 [file]"
+fi
+
+YCM_ROOT=$(readlink --canonicalize --no-newline $(dirname $1)/..)
+FILE=${YCM_ROOT}/modules/YCMEPHelper.cmake
+MODULES=$(sed -n 's/set(_ycm_\(.\+\)_sha1sum \+[0-9a-f]\+)/\1/p' ${FILE})
+
+for mod in ${MODULES}; do
+  file=$(find ${YCM_ROOT}/modules ${YCM_ROOT}/cmake-next ${YCM_ROOT}/tools -name $mod.cmake)
+  old_sha1sum=$(sed -n "s/set(_ycm_${mod}_sha1sum \+\([0-9a-f]\+\))/\1/p" ${FILE})
+  new_sha1sum=$(sha1sum $file | head -n1 | awk '{print $1}')
+  if [ "${old_sha1sum}" != "${new_sha1sum}" ]; then
+    echo "File ${file} has wrong sha1sum"
+    echo " OLD: ${old_sha1sum}"
+    echo " NEW: ${new_sha1sum}"
+    exit 1
+  fi
+done
