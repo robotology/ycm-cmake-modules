5e3d24047d76ae42467ee253ba0f6b9774c43ec0
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-03-21 01:40:16 +0100

    Better detection of the module to check the hashes

--- tools/update_sha1sums.sh
+++ tools/update_sha1sums.sh
@@ -5,9 +5,9 @@ FILE=${YCM_ROOT}/modules/YCMEPHelper.cmake
 MODULES=$(sed -n 's/set(_ycm_\(.\+\)_sha1sum \+[0-9a-f]\+)/\1/p' ${FILE})
 
 for mod in ${MODULES}; do
-  file=$(find ${YCM_ROOT} -name $mod.cmake)
+  file=$(find ${YCM_ROOT}/modules ${YCM_ROOT}/cmake-next ${YCM_ROOT}/tools -name $mod.cmake)
   old_sha1sum=$(sed -n "s/set(_ycm_${mod}_sha1sum \+\([0-9a-f]\+\))/\1/p" ${FILE})
-  new_sha1sum=$(sha1sum $file | awk '{print $1}')
+  new_sha1sum=$(sha1sum $file | head -n1 | awk '{print $1}')
 
   if [ "${old_sha1sum}" != "${new_sha1sum}" ]; then
     sed -i "s/${old_sha1sum}/${new_sha1sum}/g" ${FILE}
