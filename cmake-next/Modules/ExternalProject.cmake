e69585390202f33f4a09472bab120038928565f7
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-05-30 18:49:37 +0200

    ExternalProject: Fix SCM_DISCONNECTED patch
    Always perform the skip-update target, forcing following steps to be
    executed. This causes the configure, build, and install steps to be
    executed and therefore changes to subprojects are now correctly
    detected.
    
    This also means that since the steps are now executed every time, build
    takes more time and prints more unuseful output. The alternative is to
    have a target to force the build when something changes, but it does not
    seem to me a sufficiently generic solution.
    
    Fixes #26

--- cmake-next/Modules/ExternalProject.cmake
+++ cmake-next/Modules/ExternalProject.cmake
@@ -1944,21 +1944,16 @@ Update to Mercurial >= 2.1.1.
 
   if(always AND scm_disconnected)
     _ep_get_step_stampfile(${name} skip-update skip-update_stamp_file)
-    _ep_get_step_stampfile(${name} update update_stamp_file)
     string(REPLACE "Performing" "Skipping" comment "${comment}")
     ExternalProject_Add_Step(${name} skip-update
       COMMENT ${comment}
-      COMMAND ${CMAKE_COMMAND} -E touch ${skip-update_stamp_file}
       ALWAYS 1
       EXCLUDE_FROM_MAIN 1
       WORKING_DIRECTORY ${work_dir}
       DEPENDEES download
       ${log}
     )
-    add_custom_command(APPEND
-      OUTPUT ${update_stamp_file}
-      COMMAND ${CMAKE_COMMAND} -E remove -f ${skip-update_stamp_file}
-      )
+    set_property(SOURCE ${skip-update_stamp_file} PROPERTY SYMBOLIC 1)
   endif()
 
 endfunction()
