032714fafc4fdf4887310f0296bfb641b72e2f5e
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-07-28 16:08:04 +0200

    YCMEPHelper: Add a couple of variables to HG Bitbucket style

--- modules/YCMEPHelper.cmake
+++ modules/YCMEPHelper.cmake
@@ -389,10 +389,13 @@ function(_YCM_SETUP_HG)
     endif()
 
     # TYPE HG STYLE BITBUCKET
+    set(YCM_HG_BITBUCKET_USERNAME "" CACHE STRING "Name and email to use for Bitbucket hg repositories")
+    set(YCM_HG_BITBUCKET_COMMIT_USERNAME "" CACHE STRING "Name to use for git commits for Butbucket hg repositories (if empty will use YCM_GIT_COMMIT_NAME <YCM_GIT_COMMIT_EMAIL>)")
     set(YCM_HG_BITBUCKET_BASE_ADDRESS "https://bitbucket.org/" CACHE STRING "Address to use for Bitbucket mercurial repositories")
     set_property(CACHE YCM_HG_BITBUCKET_BASE_ADDRESS PROPERTY STRINGS "https://bitbucket.org/"
                                                                       "ssh://hg@bitbucket.org/")
     mark_as_advanced(YCM_HG_BITBUCKET_USERNAME
+                     YCM_HG_BITBUCKET_COMMIT_USERNAME
                      YCM_HG_BITBUCKET_BASE_ADDRESS)
 
 endfunction()
