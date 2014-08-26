fad1e58095d9f870e19ebcbcd795db5fe8e3e957
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-08-26 18:01:41 +0200

    Clean and improve FindFTDI and move it in conf folder

--- find-modules/FindFTDI.cmake
+++ find-modules/FindFTDI.cmake
@@ -1,40 +1,34 @@
-# Copyright (C) 2010 Ze Ji 
+# Copyright (C) 2010 Ze Ji
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
- 
+
 # Try to find ftdi
 # Once done this will define
 #
 #  FTDI_FOUND - system has ftdi
-#  FTDI_INCLUDE_DIR - ~ the ftdi include directory 
+#  FTDI_INCLUDE_DIR - ~ the ftdi include directory
 #  FTDI_LIBRARY - Link these to use ftdi
 #
 #  2010, Ze Ji
 #
 
-FIND_PATH(FTDI_INCLUDE_DIR 
-NAMES   ftdi.h
-PATHS   /usr/local/include
-        /usr/include
-        /usr/include/libftdi
-        /usr/local/include/libftdi
-)
+include(FindPackageHandleStandardArgs)
 
+find_path(FTDI_INCLUDE_DIR
+          NAMES ftdi.h
+          PATHS /usr/local/include
+                /usr/include
+                /usr/include/libftdi
+                /usr/local/include/libftdi)
 
-FIND_LIBRARY(FTDI_LIBRARY
-NAMES ftdi
-PATHS /usr/lib
-      /usr/local/lib
-)
+find_library(FTDI_LIBRARY
+             NAMES ftdi
+             PATHS /usr/lib
+                   /usr/local/lib)
 
-IF (FTDI_LIBRARY)
-    set(FTDI_FOUND TRUE)
-ELSE (FTDI_LIBRARY)
-    set(FTDI_FOUND FALSE)    
-ENDIF (FTDI_LIBRARY)
+find_package_handle_standard_args(FTDI DEFAULT_MSG FTDI_LIBRARY)
 
-set(FTDI_INCLUDE_DIR
-    ${FTDI_INCLUDE_DIR}
-)
-
-#MESSAGE("----------ftdi: ${FTDI_INCLUDE_DIR} ftdilib: ${FTDI_LIBRARY}")
-
+# Set package properties if FeatureSummary was included
+if(COMMAND set_package_properties)
+    set_package_properties(FTDI PROPERTIES DESCRIPTION "libFTDI - FTDI USB driver with bitbang mode")
+    set_package_properties(FTDI PROPERTIES URL "http://www.intra2net.com/en/developer/libftdi/")
+endif()
