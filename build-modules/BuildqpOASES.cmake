51ef4ad077bffa9d9c2ac64672b3ccebd7281365
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-10-06 11:21:30 +0200

    Add documentation

--- build-modules/BuildqpOASES.cmake
+++ build-modules/BuildqpOASES.cmake
@@ -1,3 +1,23 @@
+#.rst:
+# BuildECM
+# --------
+#
+# qpOASES
+
+#=============================================================================
+# Copyright 2014 iCub Facility, Istituto Italiano di Tecnologia
+#   Authors: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of YCM, substitute the full
+#  License text for the above reference.)
+
 # TYPE SVN STYLE COIN_OR
 set(YCM_SVN_COIN_OR_USERNAME "" CACHE STRING "Username to use for coin-or svn repositories")
 set(YCM_SVN_COIN_OR_PASSWORD "" CACHE STRING "Password to use for coin-or svn repositories")
