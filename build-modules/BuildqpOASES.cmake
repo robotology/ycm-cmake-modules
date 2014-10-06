83ba10adefda18cc54ae1f75772d3097e75972f4
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-10-06 10:41:27 +0200

    Add BuildqpOASES module

---
+++ build-modules/BuildqpOASES.cmake
@@ -0,0 +1,15 @@
+# TYPE SVN STYLE COIN_OR
+set(YCM_SVN_COIN_OR_USERNAME "" CACHE STRING "Username to use for coin-or svn repositories")
+set(YCM_SVN_COIN_OR_PASSWORD "" CACHE STRING "Password to use for coin-or svn repositories")
+set(YCM_SVN_COIN_OR_BASE_ADDRESS "https://projects.coin-or.org/svn/" CACHE INTERNAL "Address to use for coin-or svn repositories")
+mark_as_advanced(YCM_SVN_COIN_OR_USERNAME YCM_SVN_COIN_OR_PASSWORD)
+
+# qpOASES
+include(YCMEPHelper)
+
+ycm_ep_helper(qpOASES TYPE SVN
+                      STYLE COIN_OR
+                      REPOSITORY qpOASES/stable/3.0
+                      TRUST_CERT 1
+                      COMPONENT external
+                      INSTALL_COMMAND "")
