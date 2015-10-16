817a8c7541bbb074df019f50ea2ef4cab56727bd
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-10-16 22:58:25 +0200

    Import FindESDCANAPI improvements from iCub
    Import commit
      robotology/icub-main@f7e3baf0d4ede2219801be376c598a2f3082c50c

--- find-modules/FindESDCANAPI.cmake
+++ find-modules/FindESDCANAPI.cmake
@@ -25,33 +25,65 @@
 #  License text for the above reference.)
 
 
-if(NOT ESDCANAPI_FOUND)
-    set(ESDCANAPI_DIR $ENV{ESDCANAPI_DIR} CACHE PATH "Path to ESDCANAPI")
+IF(NOT ESDCANAPI_FOUND)
+    # ###########################################################################
+	#we look for the lib, give priority to ESDCANAPI_DIR which should point to
+	#the correct place, but also look in other locations. CAnSdkDir should be set
+	#by the installer.
+    SET(ESDCANAPI_DIR $ENV{ESDCANAPI_DIR} CACHE PATH "Path to ESDCANAPI")
 
-    if(WIN32)
-        # we look for the lib, give priority to ESDCANAPI_DIR which should point
-        # to the correct place, but also look in other locations. CAnSdkDir
-        # should be set by the installer.
-        find_library(ESDCANAPI_LIB ntcan ${ESDCANAPI_DIR}/develop/vc
-                                         ${CanSdkDir}/develop/vc
-                                         "C:/Program Files/CAN/develop/vc"
-                                         ${ESDCANAPI_DIR}/winnt
-                                         NO_DEFAULT_PATH)
+    IF(WIN32)
+        # Add possible search paths
+        LIST(APPEND ESDCAN_LIB_DIRS ${ESDCANAPI_DIR}/develop/vc)
+        LIST(APPEND ESDCAN_LIB_DIRS ${CanSdkDir}/develop/vc)
+        LIST(APPEND ESDCAN_LIB_DIRS ${ESDCANAPI_DIR}/winnt)
 
-        find_path(ESDCANAPI_INC_DIRS ntcan.h ${ESDCANAPI_DIR}/include
-                                             ${CanSdkDir}/include
-                                             "C:/Program Files/CAN/include")
-    else()
-       find_library(ESDCANAPI_LIB ntcan ${ESDCANAPI_DIR}/lib32 ${ESDCANAPI_DIR}/lib64)
-       set(ESDCANAPI_INC_DIRS ${ESDCANAPI_DIR}/lib32)
-    endif()
+        LIST(APPEND ESDCAN_INC_DIRS ${ESDCANAPI_DIR})
+        LIST(APPEND ESDCAN_INC_DIRS ${CanSdkDir})
 
-    if(ESDCANAPI_LIB)
-       set(ESDCANAPI_FOUND TRUE)
-    else()
-       set(ESDCANAPI_FOUND FALSE)
-       set(ESDCANAPI_INC_DIRS)
-       unset(ESDCANAPI_LIB )
-    endif()
+        # Build directory names depending on system architecture
+        IF(DEFINED ENV{PROGRAMFILES})
+            LIST(APPEND ESDCAN_LIB_DIRS "$ENV{PROGRAMFILES}/CAN/develop/vc")
+            LIST(APPEND ESDCAN_LIB_DIRS "$ENV{PROGRAMFILES}/ESD/CAN/SDK/lib/VC/i386")
 
-endif(NOT ESDCANAPI_FOUND)
+            LIST(APPEND ESDCAN_INC_DIRS "$ENV{PROGRAMFILES}/CAN")
+            LIST(APPEND ESDCAN_INC_DIRS "$ENV{PROGRAMFILES}/ESD/CAN/SDK")
+        ENDIF(DEFINED ENV{PROGRAMFILES})
+        IF(DEFINED ENV{ProgramW6432})
+            LIST(APPEND ESDCAN_LIB_DIRS "$ENV{ProgramW6432}/CAN/develop/vc")
+            LIST(APPEND ESDCAN_LIB_DIRS "$ENV{ProgramW6432}/ESD/CAN/SDK/lib/VC/i386")
+
+            LIST(APPEND ESDCAN_INC_DIRS "$ENV{ProgramW6432}/CAN")
+            LIST(APPEND ESDCAN_INC_DIRS "$ENV{ProgramW6432}/ESD/CAN/SDK")
+        ENDIF(DEFINED ENV{ProgramW6432})
+
+        # Find library
+        FIND_LIBRARY(   ESDCANAPI_LIB
+                        NAMES "ntcan"
+                        PATHS ${ESDCAN_LIB_DIRS}
+                        NO_DEFAULT_PATH)
+
+        # Find include file
+        FIND_PATH(  ESDCANAPI_INC_DIRS
+                    NAMES "ntcan.h "
+                    PATHS ${ESDCAN_INC_DIRS}
+                    PATH_SUFFIXES "include")
+        ELSE(WIN32)
+           FIND_LIBRARY(ESDCANAPI_LIB ntcan
+                        ${ESDCANAPI_DIR}/lib32
+                        ${ESDCANAPI_DIR}/lib64)
+           SET(ESDCANAPI_INC_DIRS ${ESDCANAPI_DIR}/lib32)
+    ENDIF(WIN32)
+
+      # Handle the QUIETLY and REQUIRED arguments and set ESDCANAPI_FOUND to TRUE if all listed variables are TRUE
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(ESDCANAPI  DEFAULT_MSG
+                                  ESDCANAPI_LIB ESDCANAPI_INC_DIRS)
+    # ###########################################################################
+
+
+    # ###########################################################################
+    # Mark as done
+    mark_as_advanced(ESDCANAPI_LIB ESDCANAPI_INC_DIRS)
+    # ###########################################################################
+ENDIF(NOT ESDCANAPI_FOUND)
