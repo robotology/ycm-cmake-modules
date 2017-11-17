abb0a980c7079a6bdf5155bb53795b53c38b424d
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2017-11-17 11:36:24 +0100

    YCMEPHelper: Updated GITLAB_ROBOTOLOGY style to the new git address

--- modules/YCMEPHelper.cmake
+++ modules/YCMEPHelper.cmake
@@ -332,11 +332,10 @@ function(_YCM_SETUP_GIT)
     set(YCM_GIT_GITLAB_ROBOTOLOGY_USERNAME "" CACHE STRING "Username to use for IIT Robotology Gitlab git repositories")
     set(YCM_GIT_GITLAB_ROBOTOLOGY_COMMIT_NAME "" CACHE STRING "Name to use for git commits for IIT Robotology Gitlab git repositories (if empty will use YCM_GIT_COMMIT_NAME)")
     set(YCM_GIT_GITLAB_ROBOTOLOGY_COMMIT_EMAIL "" CACHE STRING "Email address to use for git commits for IIT Robotology Gitlab git repositories (if empty will use YCM_GIT_COMMIT_EMAIL)")
-    set(YCM_GIT_GITLAB_ROBOTOLOGY_BASE_ADDRESS "https://gitlab.robotology.eu/" CACHE STRING "Address to use for IIT Robotology Gitlab git repositories")
-    set_property(CACHE YCM_GIT_GITLAB_ROBOTOLOGY_BASE_ADDRESS PROPERTY STRINGS "https://gitlab.robotology.eu/"
-                                                                               "git://gitlab.robotology.eu/"
-                                                                               "ssh://git@gitlab.robotology.eu/"
-                                                                               "git@gitlab.robotology.eu:"
+    set(YCM_GIT_GITLAB_ROBOTOLOGY_BASE_ADDRESS "https://git.robotology.eu/" CACHE STRING "Address to use for IIT Robotology Gitlab git repositories")
+    set_property(CACHE YCM_GIT_GITLAB_ROBOTOLOGY_BASE_ADDRESS PROPERTY STRINGS "https://git.robotology.eu/"
+                                                                               "ssh://git@git.robotology.eu/"
+                                                                               "git@git.robotology.eu:"
                                                                                "robotology:")
     mark_as_advanced(YCM_GIT_GITLAB_ROBOTOLOGY_USERNAME
                      YCM_GIT_GITLAB_ROBOTOLOGY_COMMIT_NAME
