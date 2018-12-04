28aaf3f24a6e70f531787dbb52c59ecf76ceb938
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-12-04 08:46:08 +0100

    3rd-party: Import FindGObject from robotology-dependencies fork

--- 3rdparty/qt-gstreamer.cmake
+++ 3rdparty/qt-gstreamer.cmake
@@ -19,7 +19,6 @@
 
 set(_files cmake/modules/COPYING-CMAKE-SCRIPTS            ff3ed70db4739b3c6747c7f624fe2bad70802987
            cmake/modules/FindGLIB2.cmake                  df497ba5188a8a98cb60d35a096680fd2e3140ce
-           cmake/modules/FindGObject.cmake                fc749adf95db8b0f8de9a75a13abe637c44823f1
            cmake/modules/FindGStreamerPluginsBase.cmake   75d8fbd4e20afdd624e538d935f3ebabb10c2778
            cmake/modules/MacroFindGStreamerLibrary.cmake  0b9cca5a62c8d4ccec99e875a36d1531e5a899a1)
 set(_ref a0e95b202a72b6d9e48bd1949ab6811c0f3c91c3)
@@ -39,10 +38,11 @@ Redistribution and use is allowed according to the terms of the 3-clause
 BSD license. See accompanying file COPYING.qt-gstreamer for details.
 ")
 
-# FindGStreamer.cmake requires some unmerged patches, therefore is downloaded
-# from robotology-dependencies fork
-set(_files cmake/modules/FindGStreamer.cmake              457b77fa471d29d79b11c02c88954fff6ec62456)
-set(_ref e0a9f16815d7ce23e3d9cdece5dd06dc2b588116)
+# FindGStreamer.cmake and FindGObject require some unmerged patches, therefore
+# they are downloaded from the robotology-dependencies fork
+set(_files cmake/modules/FindGStreamer.cmake              457b77fa471d29d79b11c02c88954fff6ec62456
+           cmake/modules/FindGObject.cmake                bdb778ace33b0c5a47150594fc048c69ad02a16a)
+set(_ref 383c41d310b14c328ab6d5d02f478f9cca182bfb)
 _ycm_download(3rdparty-qt-gstreamer
              "qt-gstreamer git repository (robotology-dependency fork)"
              "https://raw.githubusercontent.com/robotology-dependencies/qt-gstreamer/<REF>/<FILE>"
