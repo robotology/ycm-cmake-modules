2ec43161aaffea244672e586325edeebfb440e72
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-03-14 09:52:07 +0100

    FindACE: Add imported targets
    The new imported targets are
      * ACE::ACE (links the ACE library)
      * ACE::ACE_INLINE (INTERFACE library for ACE inlines only)
    
    The ACE::ACE target passes __ACE_INLINE__ by default, this can be
    changed by setting ACE_INLINE = OFF.
    
    Using the following variables is no longer necessary, and therefore
    their usage is deprecated.
    
    * ACE_ADDR_HAS_LOOPBACK_METHOD
    * ACE_COMPILES_WITHOUT_INLINE_RELEASE
    * ACE_COMPILES_WITHOUT_INLINE_DEBUG

--- find-modules/FindACE.cmake
+++ find-modules/FindACE.cmake
@@ -4,20 +4,28 @@
 #
 # Try to find the ACE library
 #
+# Targets set::
+#
+#   ACE::ACE (links the ACE library)
+#   ACE::ACE_INLINE (INTERFACE library for ACE inlines only)
+#
 # Variables set::
 #
-#   ACE_FOUND
-#   ACE_LIBRARIES
-#   ACE_INCLUDE_DIRS
+#   ACE_FOUND           - System has ACE library
+#   ACE_LIBRARIES       - ACE link libraries
+#   ACE_INCLUDE_DIRS    - ACE library include directories
+#   ACE_DEFINITIONS     - Additional compiler flags for ACE library
+#   ACE_VERSION         - ACE library version
+#   ACE_MAJOR_VERSION   - ACE major version
+#   ACE_MINOR_VERSION   - ACE minor version
+#   ACE_BETA_VERSION    - ACE beta version
 #
-#   ACE_VERSION
-#   ACE_MAJOR_VERSION
-#   ACE_MINOR_VERSION
-#   ACE_BETA_VERSION
+# Options variables::
 #
-#   ACE_COMPILES_WITHOUT_INLINE_RELEASE
-#   ACE_COMPILES_WITHOUT_INLINE_DEBUG
-#   ACE_ADDR_HAS_LOOPBACK_METHOD
+#   ACE_INLINE (default ON)
+#     The ACE::ACE target passes __ACE_INLINE__ by default, this can be
+#     changed by setting ACE_INLINE = OFF.
+#     The ACE_DEFINITIONS variable is also influenced by the same option.
 
 #=============================================================================
 # Copyright 2009 RobotCub Consortium
@@ -47,18 +55,22 @@ if(ACE_FOUND)
     unset(ACE_PATCH_VERSION)
     unset(ACE_TWEAK_VERSION)
 else()
-    ########################################################################
-    ##  general find
 
+    ########################################################################
+    ## Find include directory
     find_path(ACE_INCLUDE_DIR
               NAMES ace/ACE.h
               PATHS $ENV{ACE_ROOT}
                     $ENV{ACE_ROOT}/include
                     /usr/include
                     /usr/local/include
-              DOC "directory containing ace/*.h for ACE library")
+              DOC "directory containing ace/ACE.h for ACE library")
     mark_as_advanced(ACE_INCLUDE_DIR)
 
+    set(ACE_INCLUDE_DIRS ${ACE_INCLUDE_DIR})
+
+    ########################################################################
+    ## Find libraries
     find_library(ACE_ACE_LIBRARY_RELEASE
                  NAMES ACE
                        ace
@@ -91,35 +103,42 @@ else()
     select_library_configurations(ACE_ACE)
 
     set(ACE_LIBRARIES ${ACE_ACE_LIBRARY})
-    set(ACE_INCLUDE_DIRS ${ACE_INCLUDE_DIR})
+
+    ########################################################################
+    ## Definitions
+
+    if (NOT DEFINED ACE_INLINE OR ACE_INLINE)
+      set(ACE_DEFINITIONS "__ACE_INLINE__")
+    endif()
 
     ########################################################################
     ## OS-specific extra linkage
 
     # Solaris needs some extra libraries that may not have been found already
     if(CMAKE_SYSTEM_NAME STREQUAL "SunOS")
-        list(APPEND ACE_LIBRARIES socket rt nsl)
+        set(ACE_EXTRA_LIBRARIES socket rt nsl)
     endif(CMAKE_SYSTEM_NAME STREQUAL "SunOS")
 
     # ACE package doesn't specify that pthread and rt are needed, which is
     # a problem for users of GoLD.  Link pthread (just on Linux for now).
     if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-        list(APPEND ACE_LIBRARIES pthread rt)
+        set(ACE_EXTRA_LIBRARIES pthread rt)
     endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 
     # Windows needs some extra libraries
     if(WIN32 AND NOT CYGWIN)
-        list(APPEND ACE_LIBRARIES winmm)
+        set(ACE_EXTRA_LIBRARIES winmm)
     endif(WIN32 AND NOT CYGWIN)
 
     # Mingw needs some extra libraries
     if(MINGW)
-        list(APPEND ACE_LIBRARIES winmm ws2_32 wsock32)
+        set(ACE_EXTRA_LIBRARIES winmm ws2_32 wsock32)
     endif(MINGW)
 
+    list(APPEND ACE_LIBRARIES ${ACE_EXTRA_LIBRARIES})
 
     ########################################################################
-    ## finished - now just set up flags and complain to user if necessary
+    ## Set up flags and complain to user if necessary
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(ACE FOUND_VAR ACE_FOUND
                                           REQUIRED_VARS ACE_LIBRARIES ACE_INCLUDE_DIRS
@@ -157,7 +176,42 @@ endif()
 ## If ACE was found, check if some features are available
 
 if(ACE_FOUND)
+    # Create and populate the ACE::ACE target
+    add_library(ACE::ACE UNKNOWN IMPORTED)
 
+    set_target_properties(ACE::ACE PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${ACE_INCLUDE_DIRS}")
+    set_target_properties(ACE::ACE PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${ACE_INCLUDE_DIRS}")
+    set_target_properties(ACE::ACE PROPERTIES INTERFACE_LINK_LIBRARIES "${ACE_EXTRA_LIBRARIES}")
+    if (NOT DEFINED ACE_INLINE OR ACE_INLINE)
+        set_target_properties(ACE::ACE PROPERTIES INTERFACE_COMPILE_DEFINITIONS "__ACE_INLINE__")
+    endif()
+
+    if(ACE_ACE_LIBRARY_RELEASE)
+        set_property(TARGET ACE::ACE APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+        set_target_properties(ACE::ACE PROPERTIES IMPORTED_LOCATION_RELEASE "${ACE_ACE_LIBRARY_RELEASE}")
+        set_target_properties(ACE::ACE PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX")
+    endif()
+
+    if(ACE_ACE_LIBRARY_DEBUG)
+        set_property(TARGET ACE::ACE APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+        set_target_properties(ACE::ACE PROPERTIES IMPORTED_LOCATION_DEBUG "${ACE_ACE_LIBRARY_DEBUG}")
+        set_target_properties(ACE::ACE PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX")
+    endif()
+
+    # Create and populate the ACE::ACE_INLINE target
+    add_library(ACE::ACE_INLINE INTERFACE IMPORTED)
+
+    set_target_properties(ACE::ACE_INLINE PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${ACE_INCLUDE_DIRS}")
+    set_target_properties(ACE::ACE_INLINE PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${ACE_INCLUDE_DIRS}")
+    set_target_properties(ACE::ACE_INLINE PROPERTIES INTERFACE_LINK_LIBRARIES "${ACE_EXTRA_LIBRARIES}")
+    set_target_properties(ACE::ACE_INLINE PROPERTIES INTERFACE_COMPILE_DEFINITIONS "__ACE_INLINE__")
+endif()
+
+
+########################################################################
+## Compatibility with older versions
+
+if(ACE_FOUND)
     # If set, save variables for later
     if(DEFINED CMAKE_TRY_COMPILE_CONFIGURATION)
         set(_CMAKE_TRY_COMPILE_CONFIGURATION ${CMAKE_TRY_COMPILE_CONFIGURATION})
@@ -180,7 +234,6 @@ if(ACE_FOUND)
     set(CMAKE_REQUIRED_LIBRARIES ${ACE_LIBRARIES})
 
     include (CheckCXXSourceCompiles)
-
 
     # "__ACE_INLINE__" is needed in some configurations
     set(_ACE_NEEDS_INLINE_CPP "
@@ -206,15 +259,6 @@ int main(int argc, char *argv[]) {
         check_cxx_source_compiles("${_ACE_NEEDS_INLINE_CPP}" ACE_COMPILES_WITHOUT_INLINE_DEBUG)
     endif()
 
-
-    # Check for ACE_INET_Addr::is_loopback
-    if("${ACE_VERSION}" VERSION_LESS "5.4.8")
-        set(ACE_ADDR_HAS_LOOPBACK_METHOD 0)
-    else()
-        set(ACE_ADDR_HAS_LOOPBACK_METHOD 1)
-    endif()
-
-
     # Reset variables to their original values
     if(DEFINED _CMAKE_TRY_COMPILE_CONFIGURATION)
         set(CMAKE_TRY_COMPILE_CONFIGURATION ${_CMAKE_TRY_COMPILE_CONFIGURATION})
@@ -236,12 +280,7 @@ int main(int argc, char *argv[]) {
     else()
         unset(CMAKE_REQUIRED_LIBRARIES)
     endif()
-
 endif()
-
-
-########################################################################
-## Compatibility with older versions
 
 if (NOT COMMAND _FindACE_Deprecated)
   function(_FindACE_Deprecated _variable _access _value _current_list_file _stack)
@@ -253,10 +292,20 @@ if (NOT COMMAND _FindACE_Deprecated)
   set(ACE_LIBRARY ${ACE_ACE_LIBRARY})
   set(ACE_HAS_STRING_HASH 1)
 
+  # Check for ACE_INET_Addr::is_loopback
+  if("${ACE_VERSION}" VERSION_LESS "5.4.8")
+      set(ACE_ADDR_HAS_LOOPBACK_METHOD 0)
+  else()
+      set(ACE_ADDR_HAS_LOOPBACK_METHOD 1)
+  endif()
+
   variable_watch(ACE_LIBRARY_RELEASE _FindACE_Deprecated)
   variable_watch(ACE_LIBRARY_DEBUG _FindACE_Deprecated)
   variable_watch(ACE_LIBRARY _FindACE_Deprecated)
   variable_watch(ACE_HAS_STRING_HASH _FindACE_Deprecated)
+  variable_watch(ACE_ADDR_HAS_LOOPBACK_METHOD _FindACE_Deprecated)
+  variable_watch(ACE_COMPILES_WITHOUT_INLINE_RELEASE _FindACE_Deprecated)
+  variable_watch(ACE_COMPILES_WITHOUT_INLINE_DEBUG _FindACE_Deprecated)
 endif()
 
 ########################################################################
