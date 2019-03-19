582b5bca17c31ab511d6cba7b9a8111fc91e0e55
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-03-19 09:16:54 +0100

    ExternalProject: Refresh and restore patch to avoid deleting the clones

--- cmake-next/proposed/ExternalProject.cmake
+++ cmake-next/proposed/ExternalProject.cmake
@@ -1099,14 +1099,6 @@ if(NOT run)
   return()
 endif()
 
-execute_process(
-  COMMAND \${CMAKE_COMMAND} -E remove_directory \"${source_dir}\"
-  RESULT_VARIABLE error_code
-  )
-if(error_code)
-  message(FATAL_ERROR \"Failed to remove directory: '${source_dir}'\")
-endif()
-
 set(git_options)
 
 # disable cert checking if explicitly told not to do it
@@ -1116,23 +1108,94 @@ if(NOT \"x${tls_verify}\" STREQUAL \"x\" AND NOT tls_verify)
     -c http.sslVerify=false)
 endif()
 
-# try the clone 3 times in case there is an odd git clone issue
-set(error_code 1)
-set(number_of_tries 0)
-while(error_code AND number_of_tries LESS 3)
-  execute_process(
-    COMMAND \"${git_EXECUTABLE}\" \${git_options} clone ${git_clone_options} \"${git_repository}\" \"${src_name}\"
-    WORKING_DIRECTORY \"${work_dir}\"
-    RESULT_VARIABLE error_code
-    )
-  math(EXPR number_of_tries \"\${number_of_tries} + 1\")
-endwhile()
-if(number_of_tries GREATER 1)
-  message(STATUS \"Had to git clone more than once:
-          \${number_of_tries} times.\")
+if(EXISTS \"${source_dir}\")
+  if(NOT IS_DIRECTORY \"${source_dir}\")
+    # FIXME Perhaps support symbolic links?
+    message(FATAL_ERROR \"\\\"${source_dir}\\\" exists and is not a git repository. Remove it and try again\")
+  elseif(NOT IS_DIRECTORY \"${source_dir}/.git\")
+    file(GLOB files \"${source_dir}/*\")
+    list(LENGTH files nfiles)
+    if(nfiles)
+      message(FATAL_ERROR \"\\\"${source_dir}\\\" folder exists and is not a git repository. Remove it and try again\")
+    endif()
+  else()
+    # Already initialized git repository: no need to clone again
+    execute_process(
+      COMMAND \"${git_EXECUTABLE}\" config --local --get remote.origin.url
+      WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+      OUTPUT_VARIABLE origin_url
+      RESULT_VARIABLE error_code
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+    if(error_code)
+      message(FATAL_ERROR \"Failed to get origin remote url in: '${work_dir}/${src_name}'\")
+    endif()
+    if(\"\${origin_url}\" STREQUAL \"${git_repository}\")
+      message(STATUS \"Avoiding repeated git clone, repository already exists\")
+      return()
+    else()
+      string(TIMESTAMP now \"%Y%m%d%H%M%S\")
+      message(WARNING \"Repository URL is different. Renaming 'origin' remote to 'origin.\${now}'\")
+      execute_process(
+        COMMAND \"${git_EXECUTABLE}\" remote rename origin origin.\${now}
+        WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+        RESULT_VARIABLE error_code
+        )
+      if(error_code)
+        message(FATAL_ERROR \"Failed to rename remote in: '${work_dir}/${src_name}'\")
+      endif()
+
+      execute_process(
+        COMMAND \"${git_EXECUTABLE}\" remote add origin \"${git_repository}\"
+        WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+        RESULT_VARIABLE error_code
+        )
+      if(error_code)
+        message(FATAL_ERROR \"Failed to add origin remote in: '${work_dir}/${src_name}'\")
+      endif()
+
+      # try the fetch 3 times incase there is an odd git fetch issue
+      set(error_code 1)
+      set(number_of_tries 0)
+      while(error_code AND number_of_tries LESS 3)
+        execute_process(
+          COMMAND \"${git_EXECUTABLE}\" \${git_options} fetch \${git_clone_options} origin
+          WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+          RESULT_VARIABLE error_code
+          )
+        math(EXPR number_of_tries \"\${number_of_tries} + 1\")
+      endwhile()
+      if(number_of_tries GREATER 1)
+        message(STATUS \"Had to git fetch more than once:
+                \${number_of_tries} times.\")
+      endif()
+      if(error_code)
+        message(FATAL_ERROR \"Failed to fetch in: '${work_dir}/${src_name}'\")
+      endif()
+    endif()
+  endif()
 endif()
-if(error_code)
-  message(FATAL_ERROR \"Failed to clone repository: '${git_repository}'\")
+
+# Now perform the clone if still required
+if(NOT IS_DIRECTORY \"${source_dir}/.git\")
+  # try the clone 3 times in case there is an odd git clone issue
+  set(error_code 1)
+  set(number_of_tries 0)
+  while(error_code AND number_of_tries LESS 3)
+    execute_process(
+      COMMAND \"${git_EXECUTABLE}\" \${git_options} clone ${git_clone_options} \"${git_repository}\" \"${src_name}\"
+      WORKING_DIRECTORY \"${work_dir}\"
+      RESULT_VARIABLE error_code
+      )
+    math(EXPR number_of_tries \"\${number_of_tries} + 1\")
+  endwhile()
+  if(number_of_tries GREATER 1)
+    message(STATUS \"Had to git clone more than once:
+            \${number_of_tries} times.\")
+  endif()
+  if(error_code)
+    message(FATAL_ERROR \"Failed to clone repository: '${git_repository}'\")
+  endif()
 endif()
 
 execute_process(
