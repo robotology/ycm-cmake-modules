93278dc1fed3b66f2d4a8429ef4d8875400b4309
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-07-30 16:24:08 +0200

    Fix swig_add_library test when swig is not available

--- tests/RunCMake/cmake-next/swig_add_library.cmake
+++ tests/RunCMake/cmake-next/swig_add_library.cmake
@@ -1,9 +1,12 @@
 find_package(SWIG QUIET)
 if(SWIG_FOUND)
+  if(NOT EXISTS "${SWIG_USE_FILE}")
+    message(FATAL_ERROR "SWIG_USE_FILE not found (${SWIG_USE_FILE})")
+  endif()
   include(${SWIG_USE_FILE})
-endif()
 
-enable_language(C)
-swig_add_library(test_swig_add_library
-                 LANGUAGE python
-                 SOURCES swig_add_library.i)
+  enable_language(C)
+  swig_add_library(test_swig_add_library
+                   LANGUAGE python
+                   SOURCES swig_add_library.i)
+endif()
