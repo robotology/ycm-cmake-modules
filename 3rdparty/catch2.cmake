656b57402b49a42be3a5960d999fe1a2fe7fdbc1
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2020-08-07 11:47:49 +0200

    3rd-party: Update Catch2 Git Repository to commit de3a208e167c6baee6e8dc46f2c0466b311324c2
    This fixes the issue with tests in CMake 3.18.1

--- 3rdparty/catch2.cmake
+++ 3rdparty/catch2.cmake
@@ -18,9 +18,9 @@
 
 set(_files contrib/Catch.cmake                 6f80938187f2fe004db163481ad7b468c6f22ea4
            contrib/CatchAddTests.cmake         800eac1ea012cc4bea2f23924cc88ae65d2fbc76
-           contrib/ParseAndAddCatchTests.cmake a63d00cd2b8b21847419ec604e9f3cc1216c4596
+           contrib/ParseAndAddCatchTests.cmake 7ae77456d2e7a24021e38c4af1c16068ed65ef90
            LICENSE.txt                         3cba29011be2b9d59f6204d6fa0a386b1b2dbd90)
-set(_ref 229cc4823c8cbe67366da8179efc6089dd3893e9)
+set(_ref de3a208e167c6baee6e8dc46f2c0466b311324c2)
 set(_dir "${CMAKE_CURRENT_BINARY_DIR}/catch2")
 _ycm_download(3rdparty-catch2
               "Catch2 (C++ Automated Test Cases in a Header) git repository"
