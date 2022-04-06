bba082dcd2999a901b83dba1bf47005756f4a358
Silvio <silvio@traversaro.it>
2022-04-06 17:29:41 +0200

    Add files downloaded with YCM_DOWNLOAD_3RDPARTY option set to ON

--- 3rdparty/cmrc.cmake
+++ 3rdparty/cmrc.cmake
@@ -4,17 +4,19 @@
 ##############################################################################
 # CMakeRC.cmake is taken from the CMakeRC repository.
 
-set(_files CMakeRC.cmake  15109e38dddefce15028a0d0563557b2dc72446a
-           LICENSE.txt    053245749bccc40304ec4d9d0a47aea0b1c9f8f6)
-set(_ref f26f6e35a859b0ba28818dbcc0ed9b3880881cf4)
-set(_dir "${CMAKE_CURRENT_BINARY_DIR}/cmrc")
+set(_dir "${CMAKE_CURRENT_SOURCE_DIR}/cmrc")
 
-_ycm_download(3rdparty-cmrc
-              "CMakeRC (A Standalone CMake-Based C++ Resource Compiler) git repository"
-              "https://raw.githubusercontent.com/vector-of-bool/cmrc/<REF>/<FILE>"
-              ${_ref} "${_dir}" "${_files}")
+if(YCM_DOWNLOAD_3RDPARTY)
+  set(_files CMakeRC.cmake  15109e38dddefce15028a0d0563557b2dc72446a
+             LICENSE.txt    053245749bccc40304ec4d9d0a47aea0b1c9f8f6)
+  set(_ref f26f6e35a859b0ba28818dbcc0ed9b3880881cf4)
 
-file(WRITE "${_dir}/README.CMakeRC"
+  _ycm_download(3rdparty-cmrc
+                "CMakeRC (A Standalone CMake-Based C++ Resource Compiler) git repository"
+                "https://raw.githubusercontent.com/vector-of-bool/cmrc/<REF>/<FILE>"
+                ${_ref} "${_dir}" "${_files}")
+
+  file(WRITE "${_dir}/README.CMakeRC"
 "Some of the files in this folder and its subfolder come from the CMakeRC git
 repository (ref ${_ref}):
 
@@ -23,6 +25,7 @@ repository (ref ${_ref}):
 Redistribution and use is allowed according to the terms of the MIT License.
 See accompanying file COPYING.CMakeRC for details.
 ")
+endif()
 
 _ycm_install(3rdparty-cmrc FILES "${_dir}/CMakeRC.cmake"
                            DESTINATION "${YCM_INSTALL_MODULE_DIR}/3rdparty")
--- 3rdparty/vtk.cmake
+++ 3rdparty/vtk.cmake
@@ -4,17 +4,19 @@
 ##############################################################################
 # FindFFMPEG.cmake is taken from the VTK repository
 
-set(_files Copyright.txt                       73e1eb91dcdfcedf106ced4e67bc691614f0a3b3
-           CMake/FindFFMPEG.cmake              08ef5c25b33a4ee3cd4d65027412d2ef60c46281)
-set(_ref v9.0.1)
-set(_dir "${CMAKE_CURRENT_BINARY_DIR}/vtk")
+set(_dir "${CMAKE_CURRENT_SOURCE_DIR}/vtk")
 
-_ycm_download(3rdparty-vtk
-              "VTK (Visualization Toolkit) git repository"
-              "https://gitlab.kitware.com/vtk/vtk/raw/<REF>/<FILE>"
-              ${_ref} "${_dir}" "${_files}")
+if(YCM_DOWNLOAD_3RDPARTY)
+  set(_files Copyright.txt                       73e1eb91dcdfcedf106ced4e67bc691614f0a3b3
+             CMake/FindFFMPEG.cmake              08ef5c25b33a4ee3cd4d65027412d2ef60c46281)
+  set(_ref v9.0.1)
 
-file(WRITE "${_dir}/README.VTK"
+  _ycm_download(3rdparty-vtk
+                "VTK (Visualization Toolkit) git repository"
+                "https://gitlab.kitware.com/vtk/vtk/raw/<REF>/<FILE>"
+                ${_ref} "${_dir}" "${_files}")
+
+  file(WRITE "${_dir}/README.VTK"
 "Some of the files in this folder and its subfolder come from the VTK git
 repository (ref ${_ref}):
 
@@ -23,6 +25,7 @@ repository (ref ${_ref}):
 Redistribution and use is allowed according to the terms of the 3-clause
 BSD license. See accompanying file COPYING.VTK for details.
 ")
+endif()
 
 _ycm_install(3rdparty-vtk FILES "${_dir}/CMake/FindFFMPEG.cmake"
                           DESTINATION "${YCM_INSTALL_MODULE_DIR}/3rdparty")
