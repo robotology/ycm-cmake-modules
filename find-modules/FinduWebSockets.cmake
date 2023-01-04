d7c3107ec62e617bdb9bd31abf94ec5ead233f64
Silvio Traversaro <silvio.traversaro@iit.it>
2023-01-04 13:13:55 +0100

    Update FinduWebSockets.cmake

--- find-modules/FinduWebSockets.cmake
+++ find-modules/FinduWebSockets.cmake
@@ -14,6 +14,9 @@ uWebSockets::uWebSockets
 #]=======================================================================]
 
 include(FindPackageHandleStandardArgs)
+include(CMakeFindDependencyMacro)
+
+find_dependency(ZLIB REQUIRED)
 
 find_path(uWebSockets_INCLUDE_DIR WebSocket.h PATH_SUFFIXES uwebsockets uWebSockets)
 mark_as_advanced(uWebSockets_INCLUDE_DIR)
@@ -25,5 +28,7 @@ if(uWebSockets_FOUND)
       add_library(uWebSockets::uWebSockets INTERFACE IMPORTED)
       set_target_properties(uWebSockets::uWebSockets PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${uWebSockets_INCLUDE_DIR}")
+      set_target_properties(uWebSockets::uWebSockets PROPERTIES 
+        INTERFACE_LINK_LIBRARIES ZLIB::ZLIB)  
     endif()
 endif()
