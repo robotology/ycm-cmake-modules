910e23b0f5b092e9be3437298f1aec7662ee7e5d
Silvio Traversaro <silvio.traversaro@iit.it>
2023-01-04 16:17:08 +0100

    Update FinduWebSockets.cmake

--- find-modules/FinduWebSockets.cmake
+++ find-modules/FinduWebSockets.cmake
@@ -19,7 +19,11 @@ include(CMakeFindDependencyMacro)
 find_dependency(uSockets REQUIRED)
 find_dependency(ZLIB REQUIRED)
 
-find_path(uWebSockets_INCLUDE_DIR WebSocket.h PATH_SUFFIXES uwebsockets uWebSockets)
+# We do not use WebSocket.h as it has the same name of a
+# Windows system header, found for example in
+# C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/um
+find_path(uWebSockets_INCLUDE_DIR WebSocketProtocol.h PATH_SUFFIXES uWebSockets uwebsockets)
+
 mark_as_advanced(uWebSockets_INCLUDE_DIR)
 
 find_package_handle_standard_args(uWebSockets DEFAULT_MSG uWebSockets_INCLUDE_DIR)
