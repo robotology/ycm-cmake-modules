ec2f54e95072a69bf42a00c1413b7440cf4d1c6e
Silvio Traversaro <silvio@traversaro.it>
2024-11-09 15:34:33 +0100

    Switch BootstrapYCM to use FetchContent

--- internal-modules/YCMVersion.cmake
+++ internal-modules/YCMVersion.cmake
@@ -11,10 +11,7 @@ This module should not be used outside YCM.
 
 Variables defined by this module::
 
- YCM_VERSION          - Full version, including git commit and dirty state.
- YCM_VERSION_MAJOR    - YCM major version
- YCM_VERSION_MINOR    - YCM minor version
- YCM_VERSION_PATCH    - YCM patch version
+ YCM_VERSION_FULL     - Full version, including git commit and dirty state.
  YCM_VERSION_REVISION - Number of commits since latest release (git only)
  YCM_VERSION_DATE     - Date of the latest commit (git only)
  YCM_VERSION_DATE_REVISION - Number of commits since of beginning of the day
@@ -30,13 +27,8 @@ Variables defined by this module::
 
 include(GitInfo)
 
-set(YCM_VERSION_MAJOR 0)
-set(YCM_VERSION_MINOR 16)
-set(YCM_VERSION_PATCH 9)
-
 set(YCM_VERSION_API "${YCM_VERSION_MAJOR}.${YCM_VERSION_MINOR}")
 set(YCM_VERSION_SHORT "${YCM_VERSION_MAJOR}.${YCM_VERSION_MINOR}.${YCM_VERSION_PATCH}")
-set(YCM_VERSION "${YCM_VERSION_SHORT}")
 
 unset(YCM_VERSION_SOURCE)
 unset(YCM_VERSION_DIRTY)
@@ -73,14 +65,14 @@ if(DEFINED YCM_GIT_WT_HASH)
       string(APPEND YCM_VERSION_SHORT ".${YCM_VERSION_REVISION}")
     endif()
     set(YCM_VERSION_SOURCE "${_date}+git${YCM_GIT_WT_HASH_SHORT}")
-    set(YCM_VERSION "${YCM_VERSION_SHORT}-${YCM_VERSION_SOURCE}")
+    set(YCM_VERSION_FULL "${YCM_VERSION_SHORT}-${YCM_VERSION_SOURCE}")
   elseif(NOT "${YCM_GIT_WT_TAG}" STREQUAL "v${YCM_VERSION_SHORT}")
     # This is the same commit as the latest tag, but the version different
     # Probably some work in progress...
     # Add some random information.
     string(TIMESTAMP _ts "%Y%m%d")
     string(APPEND YCM_VERSION_SHORT "~${_ts}")
-    set(YCM_VERSION "${YCM_VERSION_SHORT}")
+    set(YCM_VERSION_FULL "${YCM_VERSION_SHORT}")
   else()
     # Same commit as latest tag.
     # Nothing to do
@@ -88,7 +80,7 @@ if(DEFINED YCM_GIT_WT_HASH)
   # Include information about the "dirty" status.
   if(YCM_GIT_WT_DIRTY)
     set(YCM_VERSION_DIRTY "dirty")
-    set(YCM_VERSION "${YCM_VERSION}+${YCM_VERSION_DIRTY}")
+    set(YCM_VERSION_FULL "${YCM_VERSION}+${YCM_VERSION_DIRTY}")
   endif()
 else()
   # This is not a git repository or git is missing.
@@ -97,7 +89,7 @@ else()
     # Add some random information.
     string(TIMESTAMP YCM_VERSION_PATCH "%Y%m%d")
     set(YCM_VERSION_SHORT "${YCM_VERSION_MAJOR}.${YCM_VERSION_MINOR}.${YCM_VERSION_PATCH}")
-    set(YCM_VERSION "${YCM_VERSION_SHORT}~dev")
+    set(YCM_VERSION_FULL "${YCM_VERSION_SHORT}~dev")
   else()
     # We assume that this is a release, there is not much that we can do if it's
     # not.
@@ -111,6 +103,7 @@ if(YCM_VERSION_DEBUG)
                YCM_VERSION_SHORT
                YCM_VERSION_MAJOR
                YCM_VERSION_MINOR
+               YCM_VERSION_FULL
                YCM_VERSION_PATCH
                YCM_VERSION_REVISION
                YCM_VERSION_DATE
@@ -122,4 +115,4 @@ if(YCM_VERSION_DEBUG)
   endforeach()
 endif()
 
-message(STATUS "YCM Version: ${YCM_VERSION} (${YCM_VERSION_SHORT})")
+message(STATUS "YCM Version: ${YCM_VERSION_FULL} (${YCM_VERSION_SHORT})")
